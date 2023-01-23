3649bb49aa0a96bc9d9b6d836d59ee3cef180b8e
NirwaaanAz <nirwaan.azhar-1@ou.edu>
2023-01-23 11:37:35 -0600

    added a simon says function

--- Main.java
+++ Main.java
@@ -6,17 +6,13 @@ import java.util.Scanner;
 	public class Main {
 		public static void main(String[] args)
 		 {
-		Scanner input = new Scanner (System.in);
-		final String C = "-1";
-		String user = input.nextLine();
-		String target = "Balls";
-		
-		Scanner word = new Scanner (user);
-		while (word.hasNext()) {
-			String next = word.next();
-			if (!next.equals(C))
-			System.out.println(next);
-		}
+			 Scanner input = new Scanner(System.in);
+			 System.out.println("Do you want to play simon says?");
+			 String user = input.next();
+			 
+			 if (user.equalsIgnoreCase("Yes")) {
+				 System.out.println("kys");
+			 }
 		
 		 }
 	}
